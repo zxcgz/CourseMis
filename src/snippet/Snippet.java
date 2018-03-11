11d17cca220b7101e4d59c608499dd012659afbc
zxc <zhxchao922@gmail.com>
2018-03-11 20:26:27 +0800

    第一次提交

--- src/snippet/Snippet.java
+++
@@ -1,7 +0,0 @@
-package snippet;
-
-public class Snippet {
-	//public static String server = "http://1g5131c576.iask.in/CourseMis";
-	public static String server = "http://127.0.0.1:8080/CourseMis";
-}
-
